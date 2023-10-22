4dd252aaec87ad19943b70d6eff0fa11fe699436
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2023-10-22 18:31:19 +0700

    preprosessing

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 from flask import Flask, url_for, render_template, request, jsonify, redirect
 import jinja2.exceptions
 import pandas as pd
+import mysql.connector
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from training_models_module import TrainingModel
-from preprocessing import cleaning_text, case_folding, tokenizing, stopword_removal, stemming
-
+from training_models_module import TrainingModel, PreprocessingModel, TFIDFModel
+from preprocessing import cleaning_text, case_folding, tokenizing, stopword_removal, stemming, normalisasi
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 
@@ -16,22 +18,28 @@ db_password = ''
 db_host = 'localhost'
 db_name = 'pemilu-sentimen-analysis'
 
-# Membuat koneksi ke database MySQL
-db_url = f'mysql://{db_user}:{db_password}@{db_host}/{db_name}'
-engine = create_engine(db_url)
+# Membuat koneksi ke database MySQL dengan mysql-connector-python
+db_connection = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
+)
 
+engine = create_engine('mysql+mysqlconnector://', creator=lambda: db_connection)
 Session = sessionmaker(bind=engine)
 session = Session()
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/training-data')
 def training_data():
     data = session.query(TrainingModel).all()
     return render_template('trainingData.html', data=data)
+
 
 @app.route('/upload-data-training', methods=['POST'])
 def upload_data_training():
@@ -44,6 +52,7 @@ def upload_data_training():
         if file and file.filename.endswith('.csv'):
             try:
                 session.execute(text('TRUNCATE TABLE training'))
+                session.execute(text('TRUNCATE TABLE preprocessing'))
                 session.commit()
                 df = pd.read_csv(file)
 
@@ -53,7 +62,7 @@ def upload_data_training():
                         # Membuat objek model dan menyimpannya ke database
                         data = TrainingModel(teks=row['teks'], label=row['label'], sosmed=row['sosmed'])
                         session.add(data)
-                    
+
                     session.commit()
                     return redirect(url_for('training_data'))
                 else:
@@ -66,38 +75,93 @@ def upload_data_training():
         return jsonify({'error': 'Metode HTTP tidak valid, hanya mendukung POST'})
 
 
-@app.route('/preprocessing')
-def preprocessing():
+@app.route('/preprocessing-proses')
+def preprocessing_proses():
     data = session.query(TrainingModel).all()
-
+    session.execute(text('TRUNCATE TABLE preprocessing'))
+    session.commit()
     # Melakukan preprocessing pada data
-    preprocessed_data = []
     for item in data:
         cleaned_text = cleaning_text(item.teks)
         lowercased_text = case_folding(cleaned_text)
         tokenized_text = tokenizing(lowercased_text)
-        stopwords_removed_text = stopword_removal(tokenized_text)
-        stemmed_text = stemming(stopwords_removed_text)
+        normalized_text = normalisasi(tokenized_text)
+        stopword_removed_text = stopword_removal(normalized_text)
+        stemmed_text = stemming(stopword_removed_text)
 
-        preprocessed_data.append({
-            'id': item.id,
-            'teks': (" ").join(stemmed_text),  # Menggabungkan kembali kata-kata yang telah dipreproses
-            'label': item.label,
-            'sosmed': item.sosmed
-        })
+        # save data to database
+        data = PreprocessingModel(teks=item.teks, hasil=(" ").join(stemmed_text), sosmed=item.sosmed, label=item.label)
+        session.add(data)
+        session.commit()
+    return redirect(url_for('preprocessing'))
 
-    return jsonify(preprocessed_data)
 
-    # return render_template('preprocessing.html', data=preprocessed_data)
+@app.route('/preprocessing')
+def preprocessing():
+    data = session.query(PreprocessingModel).all()
+    return render_template('preprocessing.html', data=data)
 
+
+@app.route('/tfidf-proses')
+def tfidf_proses():
+    session.execute(text('TRUNCATE TABLE tfidf'))
+    session.commit()
+    data = session.query(PreprocessingModel).all()
+
+    for item in data:
+        teks = item.hasil
+
+        if len(teks) > 0:
+            tfidf_vectorizer = TfidfVectorizer()
+            tfidf_matrix = tfidf_vectorizer.fit_transform([teks])
+
+            tfidf_scores = tfidf_matrix.toarray()[0]
+
+            for i, term in enumerate(tfidf_vectorizer.get_feature_names_out()):
+                tfidf_score = tfidf_scores[i]
+
+            # Hitung nilai IDF dari seluruh corpus (jika perlu)
+            idf_values = tfidf_vectorizer.idf_
+            tf_values = tfidf_matrix.toarray()[0]
+
+            for j, term in enumerate(tfidf_vectorizer.get_feature_names_out()):
+                tf_score = tf_values[j]
+                idf_score = idf_values[j]
+
+                tfidf_model = TFIDFModel(document_id=item.id, term=term, tf=tf_score, idf=idf_score,
+                                         tfidf=tfidf_score)
+                session.add(tfidf_model)
+
+    session.commit()
+
+    return redirect(url_for('tfidf'))
+
+@app.route('/tfidf')
+def tfidf():
+    data = session.query(TFIDFModel).all()
+    tfidf = []
+    for item in data:
+        tfidf.append(
+            {
+                'document_id': item.document_id,
+                'term': item.term,
+                'tf': item.tf,
+                'idf': item.idf,
+                'tfidf': item.tfidf
+            }
+        )
+    return jsonify(tfidf)
+    # return render_template('tfidf.html', data=data)
 
 @app.errorhandler(jinja2.exceptions.TemplateNotFound)
 def template_not_found(e):
     return not_found(e)
 
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
 import string
 import re
+import emoji
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
@@ -7,14 +9,15 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 # cleaning text
 def cleaning_text(text):
-    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'@\S+', '', text)
     text = re.sub(r'^RT[\s]+', '', text)
     text = re.sub(r'#\w+', '', text)
     text = re.sub(r'http\S+', '', text)
     text = re.sub(r'[0-9]+', '', text)
-    text = re.sub(r'\\n', '', text)
-    text = re.sub(r':', '', text)
-    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = emoji.demojize(text)
+    text = re.sub(r':\S+:', ' ', text)
+    text = text.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation)))
+
     return text
 
 
@@ -31,6 +34,30 @@ def tokenizing(text):
 # stopword removal
 def stopword_removal(text):
     stop_words = set(stopwords.words('indonesian'))
+    more_stopword = [
+        "ah", "u", "az", "gp", "maniaaaa", "unk", "yaa", "all", "in", "md", "waahh", "halooo", "bro", "x", "e", "ktp",
+        "kwkw", "se", "adi", "wni", "n", "wew", "k", "tps", "tu", "psi", "pbb", "jauuuuuhhhh", "anka",
+        "bppemilu", "sorry", "wkwkwk", "pra", "rk", "et", "bu", "nm", "wkakakaka", "wkwk", "waww", "ta", "that", "s",
+        "the", "key", "si", "simbolon", "ni", "a", "jr",
+        "lokalqn", "antonio", "make", "sense", "vs", "nasdem", "ps", "jkw", "z", "umkm", "ko", "an", "genz", "ganjarrr",
+        "atass", "buzerrr", "ji", "fake", "jkt", "dki", "h", "ikn", "framming", "poltracking", "chuakssss", "isl",
+        "pildun", "yah", "hayooo", "drunn", "wau", "ri", "wait", "and", "see", "sponsored", "by", "metrotivu",
+        "manies", "mk", "warni", "kpk", "smart", "cak", "tol", "y", "b", "wkwkkw", "ip", "hp",
+        "njir", "g", "buggy", "bukep", "aldi", "taher", "pkb", "pdfi", "nu", "one", "man", "dpr", "r", "t", "rt", "pks",
+        "yq", "hhahahhaa", "tk", "jokowiiiii", "kamuuu", "ngapainnn", "laaah", "wiiiees", "he", "rdebaru", "tohir",
+        "ektp", "hopifah", "yyess", "klw", "d", "ky", "ahy", "aje", "jan", "etes", "de", "pst", "hikhikhik", "kaésang",
+        "â", "ï", "mbak", "donny", "expectation", "ðÿœ", "siiiiih", "gemesiiiiiin", "gaissssssss", "suksesssss", "plt",
+        "sub", "jirrrr", "bet", "zaken", "tphm", "terkesan", "ngak", "puan", "maharani", "al", "ala", "nur", "děně",
+        "pp", "dct", "muh", "berau", "icdnr", "war", "bani", "j", "voc", "thok", "apk", "law", "kh", "arjun", "khan",
+        "hut", "kab", "rp", "l", "bahahahaha", "ppk", "pps", "maluku", "joel", "v", "ii", "iv", "pepabri", "dcs", "gn",
+        "iii", "igor", "spin", "pal", "liah", "nih", "lu", "segalanyaaaaaa", "pemilu", "say", "lah", "lahh", "wawww",
+        "wah", "wktotlah", "yak", "hehehe", "wkwkwkw", "tuhh", "siihh", "wow", "eh", "nak", "kak", "oke", "koq", "gps",
+        "location", "btw", "mba", "lho", "hemm", "wlwkwkw", "wkwwk", "jwbrt", "kpu", "bawaslu", "²", "²an", "dgngolkar",
+        "q", "wakìl", "i", "nkr", "at", "mf", "is", "best", "next", "right", "on", "placerisma", "xnxx", "so", "jis",
+        "dp", "rb", "hahahahahahahahah", "haam", "sos", "m", "ak", "pt", "ma", "nw", "ugm", "pj", "asn", "fyi", "uns",
+        "²ðÿ", "ðÿ", "âœœ", "megagiliran", "thm menteri", "tooo", "nee", "frp", "nelikung", "lskux", "guys"
+    ]
+    stop_words.update(more_stopword)
     return [word for word in text if word not in stop_words]
 
 
@@ -38,4 +65,16 @@ def stopword_removal(text):
 def stemming(text):
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
-    return [stemmer.stem(word) for word in text]
+    return [stemmer.stem(word) for word in text if isinstance(word, str)]
+
+
+# normalisasi
+def normalisasi(text):
+    normalisasi_kata = pd.read_csv('normalisasi.csv')
+    normalisasi_kata_dict = {}
+
+    for index, row in normalisasi_kata.iterrows():
+        if row[0] not in normalisasi_kata_dict:
+            normalisasi_kata_dict[row[0]] = row[1]
+
+    return [normalisasi_kata_dict[word] if word in normalisasi_kata_dict else word for word in text]
